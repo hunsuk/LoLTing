b325c3798dc6c4e25b29337a7377953f93e04f37
side <44570515+hunsuk@users.noreply.github.com>
2022-10-08 12:57:48 +0900

    Login server

---
+++ jwt_login/sanhak/src/main/java/LoginJwt/sanhak/SanhakApplication.java
@@ -0,0 +1,13 @@
+package LoginJwt.sanhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanhakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanhakApplication.class, args);
+	}
+
+}
---
+++ jwt_login/sanhak/src/test/java/LoginJwt/sanhak/SanhakApplicationTests.java
@@ -0,0 +1,13 @@
+package LoginJwt.sanhak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanhakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
